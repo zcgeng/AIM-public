d96e13a7ff272ec074c5ceab3c2c6ff724b2daff
zcgeng <zcg1996@qq.com>
2016-10-13 22:25:47 +0800

    I wrote a convinient shell script to debug: run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,14 @@ set -x
 
 make clean
 make
+
+# load the files into the hardisk
 dd if=$HOME/OS2016/AIM/boot/boot.bin of=$HOME/OS2016/100m.img bs=446 count=1 conv=notrunc
 dd if=$HOME/OS2016/AIM/kern/vmaim.elf of=$HOME/OS2016/100m.img seek=20480 bs=512 count=100 conv=notrunc
+
+# open a new shell to use gdb
+gnome-terminal -t "my_gdb" -x zsh -c "i386-unknown-elf-gdb $HOME/OS2016/AIM/kern/vmaim.elf -x gdb.cmd;"
+
+# now run it!
 qemu-system-i386 $HOME/OS2016/100m.img -gdb tcp::1234 -S
+
