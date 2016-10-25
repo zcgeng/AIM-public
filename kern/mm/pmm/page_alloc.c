6e994d1f3c9ab668d08cf12f2455200fc748f0c2
zcgeng <zcg1996@qq.com>
2016-10-26 01:28:51 +0800

    ADD: buddy alloc

--- kern/mm/pmm/page_alloc.c
+++ kern/mm/pmm/page_alloc.c
@@ -67,8 +67,8 @@ void buddy_init(int level) {
 }
 
 void
-buddy_delete(struct buddy * self) {
-	free(self);
+buddy_delete(struct buddy * my_buddy) {
+	free(my_buddy);
 }
 
 static inline int
@@ -88,6 +88,7 @@ next_pow_of_2(uint32_t x) {
 	return x+1;
 }
 
+/* get the page number of a tree node */
 static inline int
 _index_offset(int index, int level, int max_level) {
 	return ((index + 1) - (1 << level)) << (max_level - level);
@@ -99,15 +100,80 @@ get_buddy_index(int index){
 }
 
 static void 
-_mark_parent(struct buddy * self, int index) {
+_mark_parent(int index) {
 	for (;;) {
 		int buddy = get_buddy_index(index);
-		if (buddy > 0 && (self->tree[buddy] == NODE_USED || self->tree[buddy] == NODE_FULL)) {
+		if (buddy > 0 && (my_buddy->tree[buddy] == NODE_USED || my_buddy->tree[buddy] == NODE_FULL)) {
 			index = (index + 1) / 2 - 1;
-			self->tree[index] = NODE_FULL;
+			my_buddy->tree[index] = NODE_FULL;
 		}
 		else {
 			return;
 		}
 	}
+}
+
+int 
+buddy_alloc(int s) {
+	int size;
+	if (s==0) {
+		size = 1;
+	} else {
+		size = (int)next_pow_of_2(s);
+	}
+	int length = 1 << my_buddy->level;
+
+	if (size > length)
+		return -1;
+
+	int index = 0;
+	int level = 0;
+
+	// find size from the root
+	while (index >= 0) {
+		if (size == length) {
+			if (my_buddy->tree[index] == NODE_UNUSED) {
+				my_buddy->tree[index] = NODE_USED;
+				_mark_parent(my_buddy, index);
+				return _index_offset(index, level, my_buddy->level);
+			}
+		} else {
+			// size < length
+			switch (my_buddy->tree[index]) {
+			case NODE_USED:
+			case NODE_FULL:
+				break;
+			case NODE_UNUSED:
+				// split first
+				my_buddy->tree[index] = NODE_SPLIT;
+				my_buddy->tree[index*2+1] = NODE_UNUSED;
+				my_buddy->tree[index*2+2] = NODE_UNUSED;
+			default:
+				index = index * 2 + 1;
+				length /= 2;
+				level++;
+				continue;
+			}
+		}
+		if (index & 1) {
+			// search its buddy
+			++index;
+			continue;
+		}
+
+		/* go back to the parent node */
+		for (;;) {
+			level--;
+			length *= 2;
+			index = (index+1)/2 -1;
+			if (index < 0)
+				return -1;
+			if (index & 1) {
+				++index;
+				break;
+			}
+		}
+	}
+
+	return -1;
 }
