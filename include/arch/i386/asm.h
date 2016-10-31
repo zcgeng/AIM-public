35ddd2f8fbe7f1d06381b35358dfc4c630f355e1
zcgeng <zcg1996@qq.com>
2016-10-31 10:56:54 +0800

    ADD: asm in & out

--- include/arch/i386/asm.h
+++ include/arch/i386/asm.h
@@ -180,13 +180,17 @@ uint8_t inb(uint16_t port)
 static inline
 uint16_t inw(uint16_t port)
 {
-	return 0;
+	uint16_t data;
+	asm volatile("in %1, %0" : "=a"(data) : "d"(port));
+	return data;
 }
 
 static inline
 uint32_t inl(uint16_t port)
 {
-	return 0;
+	uint32_t data;
+	asm volatile("in %1, %0" : "=a"(data) : "d"(port));
+	return data;
 }
 
 static inline
@@ -198,6 +202,7 @@ void outb(uint16_t port, uint8_t data)
 static inline
 void outl(uint16_t port, uint8_t data)
 {
+	asm volatile("out %%eax, %%dx" : : "a"(data), "d"(port));
 }
 
 struct segdesc;
