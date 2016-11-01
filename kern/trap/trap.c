df0be81a015d23293e6f356eee0436122456814c
zcgeng <zcg1996@qq.com>
2016-11-01 19:43:29 +0800

    FIX idt

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -27,6 +27,7 @@
 #include <libc/stdio.h>
 #include <arch-mmu.h>
 #include <asm.h>
+#include <aim/panic.h>
 
 long handle_syscall(long number, ...)
 {
@@ -53,6 +54,10 @@ long handle_syscall(long number, ...)
 void handle_interrupt(int irq)
 {
 	kpdebug("<IRQ %d>\n", irq);
+}
+
+void trap_panic(int num){
+	panic("idt number not valid: %d\n", num);
 }
 
 #define INTERRUPT_GATE_32   0xE
@@ -86,65 +91,25 @@ static void set_trap(struct gatedesc *ptr, uint32_t selector, uint32_t offset, u
 	ptr->off_31_16 = (offset >> 16) & 0xFFFF;
 }
 
-void irq0();
-void irq1();
-void irq14();
-void vec0();
-void vec1();
-void vec2();
-void vec3();
-void vec4();
-void vec5();
-void vec6();
-void vec7();
-void vec8();
-void vec9();
-void vec10();
-void vec11();
-void vec12();
-void vec13();
-void vec14();
-void vecsys();
-
-void irq_empty();
-
 static void idt_init(){
 	int i;
 	for (i = 0; i < NR_IRQ; i ++) {
-		set_trap(idt + i, SEG_KCODE << 3, (uint32_t)handle_syscall, DPL_KERNEL);
+		set_trap(idt + i, SEG_KCODE << 3, (uint32_t)trap_panic, DPL_KERNEL);
 	}
-	set_trap(idt + 0, SEG_KCODE << 3, (uint32_t)vec0, DPL_KERNEL);
-	set_trap(idt + 1, SEG_KCODE << 3, (uint32_t)vec1, DPL_KERNEL);
-	set_trap(idt + 2, SEG_KCODE << 3, (uint32_t)vec2, DPL_KERNEL);
-	set_trap(idt + 3, SEG_KCODE << 3, (uint32_t)vec3, DPL_KERNEL);
-	set_trap(idt + 4, SEG_KCODE << 3, (uint32_t)vec4, DPL_KERNEL);
-	set_trap(idt + 5, SEG_KCODE << 3, (uint32_t)vec5, DPL_KERNEL);
-	set_trap(idt + 6, SEG_KCODE << 3, (uint32_t)vec6, DPL_KERNEL);
-	set_trap(idt + 7, SEG_KCODE << 3, (uint32_t)vec7, DPL_KERNEL);
-	set_trap(idt + 8, SEG_KCODE << 3, (uint32_t)vec8, DPL_KERNEL);
-	set_trap(idt + 9, SEG_KCODE << 3, (uint32_t)vec9, DPL_KERNEL);
-	set_trap(idt + 10, SEG_KCODE << 3, (uint32_t)vec10, DPL_KERNEL);
-	set_trap(idt + 11, SEG_KCODE << 3, (uint32_t)vec11, DPL_KERNEL);
-	set_trap(idt + 12, SEG_KCODE << 3, (uint32_t)vec12, DPL_KERNEL);
-	set_trap(idt + 13, SEG_KCODE << 3, (uint32_t)vec13, DPL_KERNEL);
-	set_trap(idt + 14, SEG_KCODE << 3, (uint32_t)vec14, DPL_KERNEL);
-
 	/* the system call 0x80 */
-	set_trap(idt + 0x80, SEG_KCODE << 3, (uint32_t)vecsys, DPL_USER);
-
-	set_intr(idt+32 + 0, SEG_KCODE << 3, (uint32_t)irq0, DPL_KERNEL);
-	set_intr(idt+32 + 1, SEG_KCODE << 3, (uint32_t)irq1, DPL_KERNEL);
-	set_intr(idt+32 + 14, SEG_KCODE << 3, (uint32_t)irq14, DPL_KERNEL);
+	set_trap(idt + 0x80, SEG_KERNEL_CODE << 3, (uint32_t)handle_syscall, DPL_USER);
+	set_intr(idt+32 + 0, SEG_KCODE << 3, (uint32_t)handle_interrupt, DPL_KERNEL);
 
 	/* the ``idt'' is its virtual address */
 	write_idtr(idt, sizeof(idt));
 }
+
 void trap_init(void){
 
 	// interrupt table preparation
 	idt_init();
 
-	// LAPIC & IOPAC
-    
-	// ban the outside interruptions
+	// LAPIC & IOAPAC
+	
+	// forbid the outside interruptions
 }
