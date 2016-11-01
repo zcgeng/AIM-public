3a3837aba1a633aecc1af34e0ebe77413c64d076
zcgeng <zcg1996@qq.com>
2016-11-01 20:27:20 +0800

    delete: some unsafe backdoor

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -28,6 +28,7 @@
 #include <arch-mmu.h>
 #include <asm.h>
 #include <aim/panic.h>
+#include <arch-trap.h>
 
 long handle_syscall(long number, ...)
 {
@@ -54,10 +55,6 @@ long handle_syscall(long number, ...)
 void handle_interrupt(int irq)
 {
 	kpdebug("<IRQ %d>\n", irq);
-}
-
-void trap_panic(int num){
-	panic("idt number not valid: %d\n", num);
 }
 
 #define INTERRUPT_GATE_32   0xE
@@ -145,30 +142,6 @@ static void idt_init(){
 	write_idtr(idt, sizeof(idt));
 }
 
-#define NR_IRQ_HANDLE 32
-#define NR_HARD_INTR 16
-
-struct IRQ_t {
-	void (*routine)(void);
-	struct IRQ_t *next;
-};
-
-static struct IRQ_t handle_pool[NR_IRQ_HANDLE];
-static struct IRQ_t *handles[NR_HARD_INTR];
-static int handle_count = 0;
-
-void
-add_irq_handle(int irq, void (*func)(void) ) {
-	assert(irq < NR_HARD_INTR);
-	assert(handle_count <= NR_IRQ_HANDLE);
-
-	struct IRQ_t *ptr;
-	ptr = &handle_pool[handle_count ++]; /* get a free handler */
-	ptr->routine = func;
-	ptr->next = handles[irq]; /* insert into the linked list */
-	handles[irq] = ptr;
-}
-
 void irq_handle(struct TrapFrame *tf) {
 	int irq = tf->irq;
 
@@ -177,16 +150,8 @@ void irq_handle(struct TrapFrame *tf) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		handle_syscall(tf);
-	} else if (irq < 1000) {
-		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
-	} else if (irq >= 1000) {
-		int irq_id = irq - 1000;
-		struct IRQ_t *f = handles[irq_id];
-
-		while (f != NULL) { /* call handlers one by one */
-			f->routine(); 
-			f = f->next;
-		}
+	} else{
+		handle_interrupt(irq);
 	}
 }
 
