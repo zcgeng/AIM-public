44d21489d87e35dace4e6dc460f57d4b8a07b0d1
zcgeng <zcg1996@qq.com>
2016-11-02 02:56:56 +0800

    fix some bugs

--- kern/arch/i386/arch_trap.c
+++ kern/arch/i386/arch_trap.c
@@ -117,26 +117,3 @@ void irq_handle(struct TrapFrame *tf) {
 		handle_interrupt(irq);
 	}
 }
-
-#define PORT_PIC_MASTER 0x20
-#define PORT_PIC_SLAVE  0xA0
-#define IRQ_SLAVE       2
-void i8259_init(void) {
-	/* mask all interrupts */
-	outb(PORT_PIC_MASTER + 1, 0xFF);
-	outb(PORT_PIC_SLAVE + 1 , 0xFF);
-	
-	/* start initialization */
-	outb(PORT_PIC_MASTER, 0x11);
-	outb(PORT_PIC_MASTER + 1, 32);
-	outb(PORT_PIC_MASTER + 1, 1 << 2);
-	outb(PORT_PIC_MASTER + 1, 0x3);
-	outb(PORT_PIC_SLAVE, 0x11);
-	outb(PORT_PIC_SLAVE + 1, 32 + 8);
-	outb(PORT_PIC_SLAVE + 1, 2);
-	outb(PORT_PIC_SLAVE + 1, 0x3);
-	outb(PORT_PIC_MASTER, 0x68);
-	outb(PORT_PIC_MASTER, 0x0A);
-	outb(PORT_PIC_SLAVE, 0x68);
-	outb(PORT_PIC_SLAVE, 0x0A);
-}
