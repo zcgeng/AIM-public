ec62f00bf7180a114d853d0cba9500e9b3017fbf
zcgeng <zcg1996@qq.com>
2016-11-16 04:32:10 +0800

    FIX: tidy my code, nothing changed

--- kern/init/early_init.c
+++ kern/init/early_init.c
@@ -37,6 +37,7 @@
 #include <asm.h>
 #include <aim/proc.h>
 #include <aim/memlayout.h>
+#include <aim/smp.h>
 
 static inline
 int early_devices_init(void)
@@ -134,7 +135,6 @@ extern void mpinit();
 extern void lapicinit();
 extern void picinit();
 extern void ioapicinit();
-static void startothers();
 
 void high_address_entry(){
 	allocator_init();
@@ -146,110 +146,6 @@ void high_address_entry(){
 	do_initcalls();
 	//test_syscall();
 	//test_alloc();
-	startothers();
+	smp_startup();
 	panic("succeed !");
 }
-
-int cpunum();
-// Set up CPU's kernel segment descriptors.
-// Run once on entry on each CPU.
-void seginit(void)
-{
-  struct cpu *c;
-
-  // Map "logical" addresses to virtual addresses using identity map.
-  // Cannot share a CODE descriptor for both kernel and user
-  // because it would have to have DPL_USR, but the CPU forbids
-  // an interrupt from CPL=0 to DPL=3.
-  c = &cpus[cpunum()];
-  c->gdt[SEG_KCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, 0);
-  c->gdt[SEG_KDATA] = SEG(STA_W, 0, 0xffffffff, 0);
-  c->gdt[SEG_UCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, DPL_USER);
-  c->gdt[SEG_UDATA] = SEG(STA_W, 0, 0xffffffff, DPL_USER);
-
-  // Map cpu and proc -- these are private per cpu.
-  c->gdt[SEG_KCPU] = SEG(STA_W, &c->cpu, 8, 0);
-
-  lgdt(c->gdt, sizeof(c->gdt));
-  loadgs(SEG_KCPU << 3);
-
-  // Initialize cpu-local storage.
-  //cpu = c;
-  //proc = 0;
-}
-
-// Common CPU setup code.
-static void mpmain(void)
-{
-  kprintf("cpu%d: starting\n", cpunum());
-  trap_init();       // load idt register
-  //xchg(&cpu->started, 1); // tell startothers() we're up
-	//TODO: use the upper line
-	*((uint*)0x7c00) = 1;
-	while(1);
-  //scheduler();     // start running processes
-}
-
-// Other CPUs jump here from entryother.S.
-static void mpenter(void)
-{
-  //switchkvm();
-  seginit();
-  lapicinit();
-  mpmain();
-}
-
-pde_t entrypgdir[];  // For entry.S
-void lapicstartap(uchar apicid, uint addr);
-// Start the non-boot (AP) processors.
-static void startothers(void)
-{
-  extern uchar _binary_entryother_start[], _binary_entryother_end[];
-	uint _binary_entryother_size = (uint)_binary_entryother_end - (uint)_binary_entryother_start;
-  uchar *code;
-  struct cpu *c;
-  char *stack = NULL;
-
-  // Write entry code to unused memory at 0x7000.
-  // The linker has placed the image of entryother.S in
-  // _binary_entryother_start.
-  code = P2V(0x7000);
-  memcpy(code, _binary_entryother_start, (uint)_binary_entryother_size);
-  for(c = cpus; c < cpus+ncpu; c++){
-    if(c == cpus+cpunum())  // We've started already.
-      continue;
-
-    // Tell entryother.S what stack to use, where to enter, and what
-    // pgdir to use. We cannot use kpgdir yet, because the AP processor
-    // is running in low  memory, so we use entrypgdir for the APs too.
-    stack = kmalloc(2048, 0);
-    *(void**)(code-4) = stack + 2048;
-    *(void**)(code-8) = mpenter;
-    *(int**)(code-12) = (void *) V2P(entrypgdir);
-		*((uint*)0x7c00) = 0; // TODO: remove this
-    lapicstartap(c->apicid, V2P(code));
-
-    // wait for cpu to finish mpmain()
-    // TODO: while(c->started == 0);
-		int i = 0;
-		while(*((uint*)0x7c00) != 1){
-			if((i++) % 100000 == 0)
-				kprintf("."); // I don't know why but I must do something here or other cpus won't start.
-		}
-		// TODO: I just use a strange memory position to mark one cpu is on.
-		// I need to use struct cpu to check whether it finishes mpmain
-  }
-}
-
-// The boot page table used in entry.S and entryother.S.
-// Page directories (and page tables) must start on page boundaries,
-// hence the __aligned__ attribute.
-// PTE_PS in a page directory entry enables 4Mbyte pages.
-
-__attribute__((__aligned__(PGSIZE)))
-pde_t entrypgdir[NPDENTRIES] = {
-  // Map VA's [0, 4MB) to PA's [0, 4MB)
-  [0] = (0) | PTE_P | PTE_W | PTE_PS,
-  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
-  [KERN_BASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
-};
