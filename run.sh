7926fde84bfd7b4478faa05b18aa27093ecc9536
zcgeng <zcg1996@qq.com>
2016-11-16 00:54:26 +0800

    debug

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ make
 
 # load the files into the hardisk
 dd if=$HOME/OS2016/AIM/boot/boot.bin of=$HOME/OS2016/100m.img bs=452 count=1 conv=notrunc
-dd if=$HOME/OS2016/AIM/kern/vmaim.elf of=$HOME/OS2016/100m.img seek=20480 bs=512 count=100 conv=notrunc
+dd if=$HOME/OS2016/AIM/kern/vmaim.elf of=$HOME/OS2016/100m.img seek=20480 bs=512 count=1000 conv=notrunc
 
 # open a new shell to use gdb
 gnome-terminal -t "my_gdb" -x zsh -c "i386-unknown-elf-gdb $HOME/OS2016/AIM/kern/vmaim.elf -x gdb.cmd;"
